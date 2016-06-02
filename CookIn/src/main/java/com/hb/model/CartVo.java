61012784afc57af55238a7c6de3d26e186ab7998
hb6007 <hb6007@hb6007>
2016-06-02 14:53:28 +0900

    UserVo, CartVo만듬

---
+++ CookIn/src/main/java/com/hb/model/CartVo.java
@@ -0,0 +1,87 @@
+package com.hb.model;
+
+import java.sql.Date;
+
+public class CartVo {
+
+	private int cart_idx;
+	private String id;
+	private int serial_num;
+	private int ea;
+	/**
+	 * 개당 가격
+	 */
+	private int eaPrice;
+	private Date cart_date;
+	
+	public CartVo() {}
+
+	public CartVo(int cart_idx, String id, int serial_num, int ea, int eaPrice, Date cart_date) {
+		this.cart_idx = cart_idx;
+		this.id = id;
+		this.serial_num = serial_num;
+		this.ea = ea;
+		this.eaPrice = eaPrice;
+		this.cart_date = cart_date;
+	}
+
+	public int getCart_idx() {
+		return cart_idx;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getSerial_num() {
+		return serial_num;
+	}
+
+	public int getEa() {
+		return ea;
+	}
+	/**
+	 * @return 개당 가격
+	 */
+	public int getEaPrice() {
+		return eaPrice;
+	}
+
+	public Date getCart_date() {
+		return cart_date;
+	}
+
+	public void setCart_idx(int cart_idx) {
+		this.cart_idx = cart_idx;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setSerial_num(int serial_num) {
+		this.serial_num = serial_num;
+	}
+
+	public void setEa(int ea) {
+		this.ea = ea;
+	}
+
+	/**
+	 * 개당 가격
+	 */
+	public void setEaPrice(int eaPrice) {
+		this.eaPrice = eaPrice;
+	}
+
+	public void setCart_date(Date cart_date) {
+		this.cart_date = cart_date;
+	}
+
+	@Override
+	public String toString() {
+		return "CartVo [cart_idx=" + cart_idx + ", id=" + id + ", serial_num=" + serial_num + ", ea=" + ea
+				+ ", eaPrice=" + eaPrice + ", cart_date=" + cart_date + "]";
+	}
+	
+}
---
+++ CookIn/src/main/java/com/hb/model/UserVo.java
@@ -0,0 +1,118 @@
+package com.hb.model;
+
+public class UserVo {
+	private String id;
+	private String name;
+	private int post;
+	private String main_addr;
+	private String sub_addr;
+	private String phone;
+	private String mobile;
+	private String email;
+	private int point;
+	private String pm_num;
+	
+	public UserVo() {}
+
+	public UserVo(String id, String name, int post, String main_addr, String sub_addr, String phone, String mobile,
+			String email, int point, String pm_num) {
+		this.id = id;
+		this.name = name;
+		this.post = post;
+		this.main_addr = main_addr;
+		this.sub_addr = sub_addr;
+		this.phone = phone;
+		this.mobile = mobile;
+		this.email = email;
+		this.point = point;
+		this.pm_num = pm_num;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPost() {
+		return post;
+	}
+
+	public String getMain_addr() {
+		return main_addr;
+	}
+
+	public String getSub_addr() {
+		return sub_addr;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public String getPm_num() {
+		return pm_num;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPost(int post) {
+		this.post = post;
+	}
+
+	public void setMain_addr(String main_addr) {
+		this.main_addr = main_addr;
+	}
+
+	public void setSub_addr(String sub_addr) {
+		this.sub_addr = sub_addr;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public void setPm_num(String pm_num) {
+		this.pm_num = pm_num;
+	}
+
+	@Override
+	public String toString() {
+		return "UserVo [id=" + id + ", name=" + name + ", post=" + post + ", main_addr=" + main_addr + ", sub_addr="
+				+ sub_addr + ", phone=" + phone + ", mobile=" + mobile + ", email=" + email + ", point=" + point
+				+ ", pm_num=" + pm_num + "]";
+	}
+	
+}
